6ae99157a1cf4ad8251bafaccb84f5d54eee4d0a
Tona Bissessar <ana_lisa8@hotmail.com>
2026-06-10 23:55:06 +0100

    Add styling via bootstrap for navigation bar and added shopping car t and product count visibility in shopping cart .

--- brushed_up_things/urls.py
+++ brushed_up_things/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('', include('artwork.urls')),
     path('profile/', include('profiles.urls')),
     path('accounts/', include('allauth.urls')),
+    path('cart/', include('cart.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
