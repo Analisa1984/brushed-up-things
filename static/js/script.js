42f424bb6c31a7f63ef5e3ad9337726df7ff9e74
Tona Bissessar <ana_lisa8@hotmail.com>
2026-06-10 04:39:45 +0100

    To be able to delete an artist, the icon available has an eventlistener in script.js to ensure this is what the staff member wants before going to the views delete_artist function.

--- static/js/script.js
+++ static/js/script.js
@@ -1,15 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const logoutLink = document.getElementById('logout-link');
     
-    // Check if the element exists on the current page to prevent console errors
+    const logoutLink = document.getElementById('logout-link');
     if (logoutLink) {
         logoutLink.addEventListener('click', function(event) {
             const confirmLogout = confirm('Are you sure you want to log out of Brushed up Things?');
-            
             if (!confirmLogout) {
-                // If they click 'Cancel', stop the browser from following the link
                 event.preventDefault();
             }
         });
     }
+
+    const deleteButtons = document.querySelectorAll('.delete-artist-btn');
+    deleteButtons.forEach(button => {
+        button.addEventListener('click', function (event) {
+            const confirmDelete = confirm('Are you sure you want to delete this artist?');
+            if (!confirmDelete) {
+                event.preventDefault();
+            }
+        });
+    });
+
 });
