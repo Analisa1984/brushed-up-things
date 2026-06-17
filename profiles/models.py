c26f625c8cee7c4a5f7f6a82774027251739913a
Tona Bissessar <ana_lisa8@hotmail.com>
2026-06-17 16:34:10 +0100

    Adfd country to userProfile model in profile app and makemigrations, then migrate changes.

--- profiles/models.py
+++ profiles/models.py
@@ -3,6 +3,7 @@ from django.conf import settings
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth.models import User
+from django_countries.fields import CountryField
 
 
 # Create your models here.
@@ -21,6 +22,8 @@ class UserProfile(models.Model):
         max_length=40, blank=True, null=True)
     default_county = models.CharField(max_length=80, blank=True, null=True)
     default_postcode = models.CharField(max_length=20, blank=True, null=True)
+    default_country = CountryField(
+        blank_label='Country', blank=True, null=True)
 
     def __str__(self):
         return f"{self.user.username}'s Profile"
@@ -30,4 +33,5 @@ class UserProfile(models.Model):
 def create_or_update_user_profile(sender, instance, created, **kwargs):
     if created:
         UserProfile.objects.create(user=instance)
-    instance.userprofile.save()
+    else:
+        instance.userprofile.save()
