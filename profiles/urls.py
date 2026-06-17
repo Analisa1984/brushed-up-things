6c1dd016be7c7e09272335df9f467c87e1d3346e
Tona Bissessar <ana_lisa8@hotmail.com>
2026-06-17 16:56:29 +0100

    Add trailing slashes at the end of add-artist, add-artwork.

--- profiles/urls.py
+++ profiles/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('', views.profile, name='profile'),
     path('delete/', views.delete_profile, name='delete_profile'),
     path('management/', views.staff_dashboard, name='staff_dashboard'),
-    path('management/add-artist', views.add_artist, name='add_artist'),
-    path('management/add-artwork', views.add_artwork, name='add_artwork')
+    path('management/add-artist/', views.add_artist, name='add_artist'),
+    path('management/add-artwork/', views.add_artwork, name='add_artwork')
 ]
